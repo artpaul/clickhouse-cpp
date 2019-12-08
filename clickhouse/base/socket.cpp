23b1a3907366913316589322b33a4964fe123389
artpaul <artmkin@gmail.com>
2019-12-08 18:15:20 +0500

    move include to cpp

--- clickhouse/base/socket.cpp
+++ clickhouse/base/socket.cpp
@@ -11,6 +11,7 @@
 #   include <errno.h>
 #   include <fcntl.h>
 #   include <netdb.h>
+#   include <netinet/tcp.h>
 #   include <signal.h>
 #   include <unistd.h>
 #endif
--- clickhouse/base/socket.h
+++ clickhouse/base/socket.h
@@ -13,7 +13,6 @@
 #   include <winsock2.h>
 #   include <ws2tcpip.h>
 #else
-#   include <netinet/tcp.h>
 #   include <sys/types.h>
 #   include <sys/socket.h>
 #   include <poll.h>
